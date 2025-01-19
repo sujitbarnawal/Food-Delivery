b323717394a96e80d94277f880c14e9a8c18d6dc
sujitbarnawal <sujitbarnawal4545@gmail.com>
2025-01-19 13:00:02 +0545

    used bcryptjs instead of bcrypt

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs'
 import validator from "validator";
 import transporter from "../config/nodemailer.js";
 
